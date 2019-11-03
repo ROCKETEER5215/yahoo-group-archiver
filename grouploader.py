b83918194c1e3a43ac7dbab0e2ebf8f410ebca58
Justin Wallace <rocketeer.jawswall@gmail.com>
2019-11-02 23:04:08 -0400

    Automate the task of downloading a large number of Yahoo Groups

--- grouploader.py
+++ grouploader.py
@@ -8,21 +8,25 @@ if (sys.version_info < (3, 0)):
 else:
     from urllib.parse import urlparse
 
-
 groupFilePath = "groups.txt"
 groupUrlList = []
 try:
     groupsFile = open(groupFilePath, 'r')
     groupUrlList = groupsFile.read().splitlines()
-    print(groupUrlList)
+
 except IOError as e:
     print("You need to make a groups.txt file in the same dir as grouploader.py")
+
+# get cookieT from line 1 and cookieY from line 2 of groups.txt
 cookieT = '"' + groupUrlList.pop(0) + '"'
 cookieY = '"' + groupUrlList.pop(0) + '"'
+
+# get arguments from line 3 of groups.txt
 arguments = groupUrlList.pop(0)
-print(arguments)
 print(groupUrlList)
 
+# get the Url's from groups.txt there should be one per line
+# parse the Url's to get the name of each group
 index = 0
 groupNameList = []
 for index in range(len(groupUrlList)):
@@ -34,11 +38,9 @@ for index in range(len(groupUrlList)):
     temp = path[3]
     groupNameList.append(temp)
 
+# create a subprocess to call python and run yahoo.py and push all arguments to cmd
+# Wait for the subprocess to finish with code 0 or fail then start the next one
 index = 0
 for index in range(len(groupNameList)):
     print("DOWNLOADING: " + groupNameList[index])
-    subprocess.call(['python', 'yahoo.py', '-ct', cookieT, '-cy', cookieY, groupNameList[index], arguments])
-
-
-
-
+    subprocess.call(['python38', 'yahoo.py', '-ct', cookieT, '-cy', cookieY, groupNameList[index], arguments])
